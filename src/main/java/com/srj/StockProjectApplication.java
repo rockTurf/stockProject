f699762c0bfaff16fe6ce5a9bba5b9012a2e4cfd
rockTurf <zeldafansme@126.com>
2020-02-24 11:13:53 +0800

    初始

---
+++ src/main/java/com/srj/StockProjectApplication.java
@@ -0,0 +1,16 @@
+package com.srj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.srj.web")
+public class StockProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockProjectApplication.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/srj/web/datacenter/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.srj.web.datacenter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@ResponseBody
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello SpringBoot Application";
+    }
+}
