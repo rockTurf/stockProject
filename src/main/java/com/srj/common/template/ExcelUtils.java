67cd7ff496f44d2b5d9e02fb99dcaed1d8551a1d
shiruojiang <ruojiang.shi@scvc.vip>
2020-05-02 18:23:02 +0800

    jar打包部署

--- src/main/java/com/srj/common/template/ExcelUtils.java
+++ src/main/java/com/srj/common/template/ExcelUtils.java
@@ -29,7 +29,7 @@ public class ExcelUtils {
 	 * @param data 数据
 	 * @param titleMap (key:"列名",value = "属性名")
 	 */
-	public static void exportExcel(HttpServletResponse response,
+	/*public static void exportExcel(HttpServletResponse response,
                                    String fileName, List<?> data, Map titleMap) throws Exception {
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		PoiUtil.writeExcel(data, os, titleMap);
@@ -54,13 +54,13 @@ public class ExcelUtils {
 	}
 
 
-	/**
+	*//**
 	 * 导出excel,模板形式(流方式直接下载)
 	 * @param response
 	 * @param templatePath 模板excel的路径
 	 * @param fileName 导出excel文件名
 	 * @param data 数据map
-	 */
+	 *//*
 	public static void exportExcel(HttpServletResponse response,
                                    String templatePath, String fileName, Map data) throws Exception {
 		ByteArrayOutputStream os = new ByteArrayOutputStream();;
@@ -129,11 +129,11 @@ public class ExcelUtils {
 		return bd.toPlainString();
 	}
 	
-	/**  
+	*//**
      * 使用java正则表达式去掉多余的.与0  
      * @param s  
      * @return   
-     */    
+     *//*
     public static int subZeroAndDot(String s){    
         if(s.indexOf(".") > 0){    
             s = s.replaceAll("0+?$", "");//去掉多余的0    
@@ -141,11 +141,11 @@ public class ExcelUtils {
         }    
         return Integer.parseInt(s);    
     }
-    /**  
+    *//**
      * 使用正则表达式校验是否数字
      * @param str
      * @return   
-     */ 
+     *//*
 	public boolean isNumeric(String str) {
 		Pattern pattern = Pattern.compile("[0-9]*");
 		Matcher isNum = pattern.matcher(str);
@@ -154,7 +154,7 @@ public class ExcelUtils {
 		}
 		return true;
 	}
-    
+    */
     
 
 }
