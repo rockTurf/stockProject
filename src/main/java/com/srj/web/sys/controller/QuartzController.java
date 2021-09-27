e405be2fcc291d982da697875e87ea71e07288e6
rockTurf <zeldafansme@126.com>
2021-09-27 15:25:52 +0800

    天行接口时间

--- src/main/java/com/srj/web/sys/controller/QuartzController.java
+++ src/main/java/com/srj/web/sys/controller/QuartzController.java
@@ -1,27 +1,17 @@
 package com.srj.web.sys.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.srj.common.constant.Constant;
-import com.srj.web.datacenter.model.Keyword;
 import com.srj.web.datacenter.model.News;
 import com.srj.web.datacenter.service.KeywordService;
 import com.srj.web.datacenter.service.NewsService;
-import com.srj.web.util.DateUtils;
 import com.srj.web.util.SpiderUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component
 @Lazy(false)
@@ -35,7 +25,7 @@ public class QuartzController {
 
       //天行新闻接口
       //@RequestMapping(value = "/news")
-      @Scheduled(cron = "0 0 */1 * * ?")
+      @Scheduled(cron = "20 5 /1 * * ?")
       @ResponseBody
       public void getCsStockNews(){
             List<News> list = SpiderUtils.getTianNews();
